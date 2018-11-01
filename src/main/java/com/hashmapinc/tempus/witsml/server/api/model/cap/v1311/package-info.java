98eadcc9340e4f4f1a6bfab068556fc8b7ddcb27
Chris Herrera <chris@Chriss-MacBook-Pro.local>
2018-11-01 09:25:14 -0500

    This commit resolves #29.
    License headers can be added to any compatable file using mvn license:format.
    
    Licenses will also be verified at mvn install time and the build will fail if the licences have not
     been added

--- src/main/java/com/hashmapinc/tempus/witsml/server/api/model/cap/v1311/package-info.java
+++ src/main/java/com/hashmapinc/tempus/witsml/server/api/model/cap/v1311/package-info.java
@@ -1,2 +1,17 @@
+/**
+ * Copyright © 2018-2018 Hashmap, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.witsml.org/api/131", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package com.hashmapinc.tempus.witsml.server.api.model.cap.v1311;
