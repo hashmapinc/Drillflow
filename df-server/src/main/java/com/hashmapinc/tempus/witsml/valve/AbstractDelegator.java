108b3549a1a8dd2e6aaa56935f7d505647dbcf9b
Randy Pitcher <randypitcherii@gmail.com>
2018-11-15 14:33:01 -0600

    implemented abstract delegator and abstract translator

---
+++ df-server/src/main/java/com/hashmapinc/tempus/witsml/valve/AbstractDelegator.java
@@ -0,0 +1,9 @@
+package com.hashmapinc.tempus.witsml.valve;
+
+/**
+ * This class is meant to add structure and default
+ * functionality to any valve delegator
+ */
+public abstract class AbstractDelegator {
+    
+}
---
+++ df-server/src/main/java/com/hashmapinc/tempus/witsml/valve/AbstractTranslator.java
@@ -0,0 +1,9 @@
+package com.hashmapinc.tempus.witsml.valve;
+
+/**
+ * This class is meant to add structure and default
+ * functionality to any valve translator
+ */
+public abstract class AbstractTranslator {
+
+}
