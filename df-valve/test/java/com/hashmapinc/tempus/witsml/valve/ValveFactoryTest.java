2d1dba66084226c376f26ee789d2417b875df5f2
Vijay Poojary <vijaydeveloper07@gmail.com>
2018-11-20 14:45:01 -0500

    Added test cases - MOCK, DoT and Null for ValveFactory

--- df-valve/test/java/com/hashmapinc/tempus/witsml/valve/ValveFactoryTest.java
+++ df-valve/test/java/com/hashmapinc/tempus/witsml/valve/ValveFactoryTest.java
@@ -17,16 +17,13 @@
 package com.hashmapinc.tempus.witsml.valve;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 
 
-public class ValveFactoryTest 
+
+public class ValveFactoryTest  
 {
     @Autowired
     private ValveFactory valveFactory;
