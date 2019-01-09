d52d8da89d98971fd3af7ad22101a268c93b0f44
Vijay Poojary <vijay.poojary@hashmapinc.com>
2019-01-09 13:42:31 -0500

    fixed compilation issues

--- df-valve/src/main/java/com/hashmapinc/tempus/witsml/valve/dot/DotValve.java
+++ df-valve/src/main/java/com/hashmapinc/tempus/witsml/valve/dot/DotValve.java
@@ -44,7 +44,7 @@ public class DotValve implements IValve {
         String tokenPath = config.get("token.path");
 
         this.CLIENT = new DotClient(url, apikey, tokenPath);
-        this.DELEGATOR = new DotDelegator(url);
+        this.DELEGATOR = new DotDelegator(config);
 
         LOG.info("Creating valve pointing to url: " + url);
     }
@@ -231,8 +231,9 @@ public class DotValve implements IValve {
         // supported objects for each function
         AbstractWitsmlObject well = new com.hashmapinc.tempus.WitsmlObjects.v1311.ObjWell(); // 1311 is arbitrary
         AbstractWitsmlObject wellbore = new com.hashmapinc.tempus.WitsmlObjects.v1311.ObjWellbore(); // 1311 is arbitrary
+        AbstractWitsmlObject trajectory = new com.hashmapinc.tempus.WitsmlObjects.v1311.ObjTrajectory(); // 1311 is arbitrary
         AbstractWitsmlObject[][] supportedObjects = {
-            {well, wellbore}, // ADD TO STORE OBJECTS
+            {well, wellbore, trajectory}, // ADD TO STORE OBJECTS
             {well, wellbore}, // GET FROM STORE OBJECTS
             {well, wellbore}, // DELETE FROM STORE OBJECTS
             {well, wellbore}, // UPDATE IN STORE OBJECTS
