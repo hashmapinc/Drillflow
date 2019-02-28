8ab067eca10f12cc9300e2f615ce3ff0aed56a31
Randy Pitcher <randypitcherii@gmail.com>
2019-02-28 14:25:16 -0600

    fixed the confusing dest/src notation in jsonUtil.merge

--- df-valve/src/main/java/com/hashmapinc/tempus/witsml/valve/dot/JsonUtil.java
+++ df-valve/src/main/java/com/hashmapinc/tempus/witsml/valve/dot/JsonUtil.java
@@ -25,63 +25,63 @@ import org.json.JSONObject;
 public class JsonUtil {
 
     /**
-     * This function merges the fields from src that
-     * are missing from dest. Only fields that exist in dest
+     * This function merges the fields from resp that
+     * are missing from req. Only fields that exist in req
      * but have an empty value are merged.
      *
-     * @param dest - JSONObject to merge into. Existing values here are unchanged
-     * @param src - src to merge from for blank values in dest
-     * @return dest - fully merged dest
+     * @param req - JSONObject to merge into. Existing values here are unchanged
+     * @param resp - resp to merge from for blank values in req
+     * @return req - fully merged req
      */
     public static JSONObject merge (
-        JSONObject dest,
-        JSONObject src
+        JSONObject req,
+        JSONObject resp
     ) {
         // track keys that should be removed in cleanup
         ArrayList<String> keysToRemove = new ArrayList<>();
 
         // iterate through keys and merge in place
-        Set<String> keyset = dest.keySet();
+        Set<String> keyset = req.keySet();
         for (String key : keyset) {
-            // filter out empty arrays from the dest
-            if (isEmptyArray(dest.get(key))) {
+            // filter out empty arrays from the req
+            if (isEmptyArray(req.get(key))) {
                 keysToRemove.add(key);
                 continue;
             }
             // check that the response has a value for this key.
-            if (!src.has(key)) {
-                if (isEmpty(dest.get(key)))
+            if (!resp.has(key)) {
+                if (isEmpty(req.get(key)))
                     keysToRemove.add(key); // remove this key if it's empty
                 continue;
             }
 
-            // get values in src and dest as objects for this key
-            Object destObj = dest.get(key);
-            Object srcObj = src.get(key);
+            // get values in resp and req as objects for this key
+            Object destObj = req.get(key);
+            Object srcObj = resp.get(key);
 
             // do merging below for each possible type
             if (destObj instanceof JSONObject && srcObj instanceof JSONObject ) {
                 merge((JSONObject) destObj, (JSONObject) srcObj); // recursively copy into destObj
-                dest.put(key, destObj); // update dest with the updated value for this key
+                req.put(key, destObj); // update req with the updated value for this key
 
             } else if (destObj instanceof JSONArray && srcObj instanceof JSONArray ) {
                 if (isEmpty(destObj) && !isEmpty(srcObj)) {
-                    dest.put(key, srcObj); // TODO: deep merging on sub objects
+                    req.put(key, srcObj); // TODO: deep merging on sub objects
                 }
             } else { // handle all basic values (non array, non nested objects)
-                dest.put(key, srcObj);
+                req.put(key, srcObj);
             }
 
-            // if after all the merging the dest value is still empty, add the key to list of removable fields
-            if (isEmpty(dest.get(key)))
+            // if after all the merging the req value is still empty, add the key to list of removable fields
+            if (isEmpty(req.get(key)))
                 keysToRemove.add(key);
         }
 
         // cleanup fields
-        for (String removableKey : keysToRemove) dest.remove(removableKey);
+        for (String removableKey : keysToRemove) req.remove(removableKey);
 
-        // return the dest
-        return dest;
+        // return the req
+        return req;
     }
 
     /**
