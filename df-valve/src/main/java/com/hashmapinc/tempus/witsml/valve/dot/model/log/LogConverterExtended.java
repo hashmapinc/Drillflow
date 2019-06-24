a13354a4d5cb8321d053cf54a75309c68e093e8b
sukehndu mukherjee <sukehndumukherjee@sukehndus-MacBook-Pro.local>
2019-06-24 12:01:00 -0500

    clean some code

--- df-valve/src/main/java/com/hashmapinc/tempus/witsml/valve/dot/model/log/LogConverterExtended.java
+++ df-valve/src/main/java/com/hashmapinc/tempus/witsml/valve/dot/model/log/LogConverterExtended.java
@@ -68,15 +68,7 @@ public class LogConverterExtended extends com.hashmapinc.tempus.WitsmlObjects.Ut
         ObjLog log =null;
 
         List<ChannelSet> cs = ChannelSet.jsonToChannelSetList(channelSet);
-
-/*        if (((com.hashmapinc.tempus.WitsmlObjects.v1411.ObjLog)witsmlObject).getLogData() != null){
-            log = ChannelSet.to1411LogData(cs.get(0),channels);
-        }else{
-            log = ChannelSet.to1411(cs.get(0));
-        }*/
-
         log = ChannelSet.to1411(cs.get(0));
-        
         log.setUidWell(witsmlObject.getGrandParentUid());
         log.setUidWellbore(witsmlObject.getParentUid());
         log.setNameWell(getWellName( wellSearchEndpoint,client,  username,password,  exchangeID,witsmlObject));
